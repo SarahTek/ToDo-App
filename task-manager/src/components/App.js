5894e4eb1346d32178752184f5ec123cbbf8d7f8
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-10-31 00:01:20 -0400

    working on login and auth0

--- task-manager/src/components/App.js
+++ task-manager/src/components/App.js
@@ -2,13 +2,22 @@ import React from 'react'
 import "../App.css";
 import TaskList from './TaskList.js';
 import TaskForm from './TaskForm';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { withAuth0 } from '@auth0/auth0-react';
 import TaskListContextProvider from '../contexts/TaskListContext';
 import Header from "./Header";
+import LoginButton from './Login';
+import LogoutButton from './Logout';
+import Profile from './profile';
+
 const App = () => {
   return (
     <TaskListContextProvider>
       <div className="container">
         <div className="app-wrapper">
+          <LoginButton />
+          <LogoutButton />
+          <Profile />
           <Header />
           <div className="main">
             <TaskForm />
@@ -21,4 +30,4 @@ const App = () => {
   )
 }
 
-export default App
+export default withAuth0(App);
